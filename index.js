b34c921ba33092165aee8f6558c66f04e3a94c2c
msyk <nii@msyk.net>
2022-10-01 16:22:17 +0900

    Setup index.js

--- index.js
+++ index.js
@@ -8,16 +8,23 @@
  * https://github.com/INTER-Mediator/INTER-Mediator/blob/master/dist-docs/License.txt
  */
 IMParts_Catalog.qrcode = {
+  // Using https://github.com/soldair/node-qrcode
+  options: {
+    color: {
+      dark: '#00F',  // Blue dots
+      light: '#0000' // Transparent background
+    }
+  },
+
   instanciate: function (parentNode) {
     'use strict'
     const classOfParent = parentNode.getAttribute('class')
-    if (classOfParent.indexOf('_im_widget_qrcode') < 0) {
-      const sp = classOfParent.length == 0 ? '' : ' '
+    if (!classOfParent || classOfParent.length == 0 || classOfParent.indexOf('_im_widget_qrcode') < 0) {
+      const sp = (classOfParent && classOfParent.length > 0) ? ' ' : ''
       parentNode.setAttribute('class', `${classOfParent}${sp}_im_widget_qrcode`)
       const node = document.createElement('CANVAS')
-      node.setAttribute('data-im-control', 'enclosure')
       node.setAttribute('class', '_im_widget_qrcode_canvas')
-      const newId = parentNode.getAttribute('id') + '-popupsel'
+      const newId = parentNode.getAttribute('id') + '-qrcode'
       node.setAttribute('id', newId)
       parentNode.appendChild(node)
       IMParts_Catalog.qrcode.ids.push(newId)
@@ -32,17 +39,25 @@ IMParts_Catalog.qrcode = {
       parentNode._im_setValue = (function () {
         const theId = newId
         return function (str) {
-          QRCode.toCanvas(document.getElementById(theId), str, {errorCorrectionLevel: 'H'}, function (err, canvas) {
-            if (err) throw err
-          })
+          IMParts_Catalog.qrcode.values[theId] = str
         }
       })()
     }
   },
 
   ids: [],
+  values: [],
 
   finish: function () {
     'use strict'
+    for (const id in IMParts_Catalog.qrcode.values) {
+      QRCode.toCanvas(document.getElementById(id), IMParts_Catalog.qrcode.values[id], IMParts_Catalog.qrcode.options,
+        function (err, canvas) {
+          if (err) {
+            console.log(err)
+            throw err
+          }
+        })
+    }
   }
 }
